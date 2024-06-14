0a77fe85ee2157ea7a05bdbbb010c304a6d11adc
hobe0m <beom231231@gmail.com>
2024-06-14 19:09:43 +0900

    [finish]Variable

---
+++ src/variable/Var7.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; // 논리, true / false 입력 가능
+        char d = 'A'; // 문자 하나
+        String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        // CTRL + D : 한줄 복사
+        // 변수 앞에 데이터 타입을 지정하기 때문에 그 그릇에 맞는 데이터만 넣을 수 있다.
+
+        // 리터럴 : 코드에서 개발자가 직접 적은 데이터의 값, 어원은 문자 혹은 글자
+        // 변수에 직접 집어 넣는 데이터의 값(고정된 값)을 프로그래밍 용어로 리터럴이라고 한다.
+        // a = 1;를 a = 2;로 바꾸는 것은 아예 코드 자체를 바꾸는 것이다.
+    }
+}
---
+++ src/variable/VarEx3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+
+        long l;
+        l = 10000000000L;
+        System.out.println(l);
+
+        boolean b;
+        b = true;
+        System.out.println(b);
+    }
+}
